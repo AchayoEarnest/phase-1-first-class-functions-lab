5cad81d1a34f402e532d3b5b580c760e3f068f1b
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-06-09 15:03:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+var driversArr = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (driversArr){
+    return driversArr.slice(0,2)
+}
+const returnLastTwoDrivers = function(driverArray){
+    return driverArray.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(integer){
+    return function (fare){
+        return fare * integer;
+    }
+}
+const fareDoubler = (fare) => fare* 2;
+
+
+const fareTripler = (fare)  => fare*3; 
+
+function selectDifferentDrivers(driversArr, func) {
+    return func(driversArr);
+}
